ff22fdf20970a3770d2216baed8a1f10f76336c5
Puseletso931 <127834075+Puseletso931@users.noreply.github.com>
2023-09-25 17:11:19 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'; // Import global styles
+import SortingAlgorithmsPage from './components/SortingAlgorithmsPage';
+
+
+function App() {
+  return (
+    
+              <div style={{ flex: 1 }}>
+                <SortingAlgorithmsPage />
+              </div>
+
+  );
+}
+
+export default App;
