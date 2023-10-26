93fed6fba53d06f1d56256206384983a8ca4dced
SenzoG19 <2491597@students.wits.ac.za>
2023-10-26 17:18:40 +0200

    QS-2

--- src/components/QuickSortVisualizer.js
+++ src/components/QuickSortVisualizer.js
@@ -1,18 +1,44 @@
-let QuickSortVisualizer = () => {
-  return (
-    <div>
+import React from "react";
+
+class ArrayVisualizer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      array: this.generateRandomArray(),
+    };
+  }
+
+  generateRandomArray = () => {
+    const size = Math.floor(Math.random() * 20) + 1; // Generates a random size between 1 and 20
+    const array = Array.from({ length: size }, () =>
+      Math.floor(Math.random() * 100)
+    ); // Fills the array with random numbers
+    return array;
+  };
+
+  handleClick = (item) => {
+    // Handle click logic here
+    console.log(`You clicked: ${item}`);
+  };
+
+  render() {
+    const { array } = this.state;
+
+    return (
       <div>
-        <h1>Enter your array below.</h1>
+        {array.map((item, index) => (
+          <button key={index} onClick={() => this.handleClick(item)}>
+            {item}
+          </button>
+        ))}
       </div>
-      <div>
-        <input
-          type="text"
-          placeholder="Enter values seperated by commas."
-        ></input>
-        <button>Enter</button>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+// Example usage
+function QuickSortVisualizer() {
+  return <ArrayVisualizer />;
+}
 
 export default QuickSortVisualizer;
