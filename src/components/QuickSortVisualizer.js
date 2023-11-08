4676389613f2a1533bb2fc19d589b8a680fcf41f
SenzoG19 <2491597@students.wits.ac.za>
2023-11-08 17:29:46 +0200

    added random array functionality

--- src/components/QuickSortVisualizer.js
+++ src/components/QuickSortVisualizer.js
@@ -6,7 +6,34 @@ const sleep = (milliseconds) => {
 };
 
 const QuicksortVisualizer = () => {
-  const [array, setArray] = useState([7, 2, 1, 6, 8, 5, 3, 4]);
+  const generateRandomArray = (length) => {
+    const array = [];
+    for (let i = 0; i < length; i++) {
+      array.push(Math.floor(Math.random() * 100) + 1);
+    }
+    return array;
+  };
+
+  const [array, setArray] = useState(generateRandomArray(8));
+  const [inputArray, setInputArray] = useState("");
+  const [highlightedIndices, setHighlightedIndices] = useState([]);
+
+  const handleChange = (event) => {
+    setInputArray(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (inputArray) {
+      const newArray = inputArray.split(",").map((item) => parseInt(item.trim()));
+      setArray(newArray);
+      sortArray(newArray, 0, newArray.length - 1);
+    } else {
+      const newArray = generateRandomArray(8);
+      setArray(newArray);
+      sortArray(newArray, 0, newArray.length - 1);
+    }
+  };
 
   const quickSort = async (arr, left, right) => {
     if (left >= right) return;
@@ -21,10 +48,13 @@ const QuicksortVisualizer = () => {
     const pivot = arr[right];
     let i = left - 1;
     for (let j = left; j < right; j++) {
+      setHighlightedIndices([j, right]);
+      await sleep(500);
       if (arr[j] < pivot) {
         i++;
         [arr[i], arr[j]] = [arr[j], arr[i]];
         setArray([...arr]);
+        setHighlightedIndices([i, j]);
         await sleep(500);
       }
     }
@@ -32,26 +62,38 @@ const QuicksortVisualizer = () => {
     [arr[i], arr[right]] = [arr[right], arr[i]];
     setArray([...arr]);
     await sleep(500);
+    setHighlightedIndices([]);
     return i;
   };
 
-  useEffect(() => {
-    const sortArray = async () => {
-      await quickSort(array, 0, array.length - 1);
-    };
-    sortArray();
-  }, []);
+  const sortArray = (newArray, left, right) => {
+    quickSort(newArray, left, right);
+  };
 
   return (
-    <div style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
-      {array.map((value, idx) => (
-        <div
-          key={idx}
-          className={`circle ${idx === array.length - 1 ? "highlight" : ""}`}
-        >
-          {value}
-        </div>
-      ))}
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter Comma Separated Numbers:
+          <input type="text" value={inputArray} onChange={handleChange} />
+        </label>
+        <button type="submit">Sort</button>
+        <button type="submit" onClick={handleSubmit} className="random-button">
+          Generate Random Array
+        </button>
+      </form>
+      <div style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
+        {array.map((value, idx) => (
+          <div
+            key={idx}
+            className={`circle ${idx === array.length - 1 ? "highlight" : ""} ${
+              highlightedIndices.includes(idx) ? "ring" : ""
+            }`}
+          >
+            {value}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
