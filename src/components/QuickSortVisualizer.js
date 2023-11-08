30bfa6cb43617304b31ca3b66661fc582d472e00
SenzoG19 <2491597@students.wits.ac.za>
2023-11-08 15:32:23 +0200

    Rework of the visualizer

--- src/components/QuickSortVisualizer.js
+++ src/components/QuickSortVisualizer.js
@@ -1,44 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./styles.css";
 
-class ArrayVisualizer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      array: this.generateRandomArray(),
+const sleep = (milliseconds) => {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
+
+const QuicksortVisualizer = () => {
+  const [array, setArray] = useState([7, 2, 1, 6, 8, 5, 3, 4]);
+
+  const quickSort = async (arr, left, right) => {
+    if (left >= right) return;
+    let index = await partition(arr, left, right);
+    await Promise.all([
+      quickSort(arr, left, index - 1),
+      quickSort(arr, index + 1, right),
+    ]);
+  };
+
+  const partition = async (arr, left, right) => {
+    const pivot = arr[right];
+    let i = left - 1;
+    for (let j = left; j < right; j++) {
+      if (arr[j] < pivot) {
+        i++;
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+        setArray([...arr]);
+        await sleep(500);
+      }
+    }
+    i++;
+    [arr[i], arr[right]] = [arr[right], arr[i]];
+    setArray([...arr]);
+    await sleep(500);
+    return i;
+  };
+
+  useEffect(() => {
+    const sortArray = async () => {
+      await quickSort(array, 0, array.length - 1);
     };
-  }
+    sortArray();
+  }, []);
 
-  generateRandomArray = () => {
-    const size = Math.floor(Math.random() * 20) + 1; // Generates a random size between 1 and 20
-    const array = Array.from({ length: size }, () =>
-      Math.floor(Math.random() * 100)
-    ); // Fills the array with random numbers
-    return array;
-  };
+  return (
+    <div style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
+      {array.map((value, idx) => (
+        <div
+          key={idx}
+          className={`circle ${idx === array.length - 1 ? "highlight" : ""}`}
+        >
+          {value}
+        </div>
+      ))}
+    </div>
+  );
+};
 
-  handleClick = (item) => {
-    // Handle click logic here
-    console.log(`You clicked: ${item}`);
-  };
-
-  render() {
-    const { array } = this.state;
-
-    return (
-      <div>
-        {array.map((item, index) => (
-          <button key={index} onClick={() => this.handleClick(item)}>
-            {item}
-          </button>
-        ))}
-      </div>
-    );
-  }
-}
-
-// Example usage
-function QuickSortVisualizer() {
-  return <ArrayVisualizer />;
-}
-
-export default QuickSortVisualizer;
+export default QuicksortVisualizer;
