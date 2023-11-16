ac8da412a98446ce2f3e1663fcc41caaed73e362
Puseletso931 <127834075+Puseletso931@users.noreply.github.com>
2023-11-16 09:32:27 +0200

    Update MergeQuizzesPage.js

--- src/components/sorts/MergeQuizzesPage.js
+++ src/components/sorts/MergeQuizzesPage.js
@@ -1,23 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import './MergeQuizzesPage.css'; 
-import { getFirestore, collectionGroup, collection, getDocs, query, where } from 'firebase/firestore';
-import Header from '../header/Header';
+import React, { useState,useEffect } from 'react';
+import QuizzesList from './QuizzesList';
 import DBQuizzesList from './DBQuizzesList';
-const MergeQuizzesPage = () => {
-  const [quizzes, setQuizzes] = useState([]);
-  const [quizQuestions, setQuizQuestions] = useState([]);
-  const [selectedQuiz, setSelectedQuiz] = useState(null);
+import { getFirestore,collectionGroup, collection, getDocs, or } from 'firebase/firestore';
+import { query, where,orderBy } from 'firebase/firestore';
+import Header from '../header/Header';
+import './MergeQuizzesPageStyle.css';
 
-  
+const  MergeQuizzesPage= () => {
+  const [quizzes, setQuizzes] = useState([]);
+  const [selectedQuiz, setSelectedQuiz] = useState(null);
+  const [quizQuestions, setQuizQuestions] = useState([]);
+
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState({});
   const [quizStarted, setQuizStarted] = useState(false);
+  const [quizSubmitted, setQuizSubmitted] = useState(false);
 
   useEffect(() => {
     const fetchQuizzes = async () => {
       const db = getFirestore();
       const quizzesCol = collection(db, 'Quizzes');
-      const q = query(quizzesCol, where('Sort Type', '==', 'Merge'));
+
+      // Create a query to get documents from the "Quizzes" collection
+      const q = query(quizzesCol, where('Sort Type', '==', 'Merge'), orderBy('Quiz ID', 'asc'));
 
       try {
         const querySnapshot = await getDocs(q);
@@ -31,65 +36,110 @@ const MergeQuizzesPage = () => {
         console.error('Error getting quizzes: ', error);
       }
     };
-
-    fetchQuizzes();
+    
+    fetchQuizzes(); // Call the function to fetch quizzes when the component mounts
+    console.log("Selected Quiz: "+selectedQuiz);
+    console.log(selectedQuiz);
+    // Call the function to fetch quiz questions when the component mounts
   }, []);
+  console.log(quizzes);
+
   useEffect(() => {
     console.log('Updated quiz questions:', quizQuestions);
     // You can perform any additional actions here after quizQuestions is updated
   }, [quizQuestions]);
+  const fetchQuizQuestions = async (QuizID) => {
+    const db=getFirestore();
+    const demoQuestions=collection(db,'Sorts','Merge',QuizID.toString()); //REPLACE "0" WITH QUIZ ID
+    //const demoQuestions = query(collectionGroup(db, '0'));
 
-  const fetchQuizQuestions = async (quizTitle, filter) => {
-    const db = getFirestore();
-    const quizQuestionsQuery = query(
-      collectionGroup(db, quizTitle),
-      where('Title', '==', filter)
-    );
-
-    try {
-      const querySnapshot = await getDocs(quizQuestionsQuery);
-      const data = querySnapshot.docs.map((doc) => ({
-        questionId: doc.id,
-        question: doc.data()['Question'],
-        answer: doc.data()['Answer'],
-        explanation: doc.data()['Explanation'],
-        options: [
-          doc.data()['Option A'],
-          doc.data()['Option B'],
-          doc.data()['Option C'],
-          doc.data()['Option D'],
-        ],
-      }));
-      setQuizQuestions(data);
-      console.log('quiz questions inside fetch:', data);
-    } catch (error) {
-      console.error("Error getting quiz questions: ", error);
+    try{
+    const querySnapshot = await getDocs(demoQuestions);
+    const data = querySnapshot.docs.map((doc) => ({
+      questionId: doc.id,
+      question: doc.data()['Question'],
+      answer: doc.data()['Answer'],
+      explanation: doc.data()['Explanation'],
+      options:[doc.data()['Option A'],doc.data()['Option B'],doc.data()['Option C'],doc.data()['Option D']],
+    }));
+    setQuizQuestions(data);
+  }catch(error){
+      console.log("Error getting quiz questions: ",error);
     }
   };
+    const onStartQuiz = async (quizId) => {
+    const selected = quizzes.find(quiz => quiz.quizId=== quizId);
 
-  const onStartQuiz = async (quizId) => {
-    console.log('Starting quiz', quizId);
-    window.alert('Starting quiz: ' + quizId);
-    const selected = quizzes.find((quiz) => quiz.quizId === quizId);
-
+    const currentQ = quizQuestions[currentQuestionIndex]; //change index to currentQuestionIndex
+    console.log("Selected: "+selected);
+    console.log(selected);
+    console.log("Quiz ID: "+(selected.quizId).toString());
     if (selected && selected.quizId === 0) {
-      const { sortType, title } = selected;
-      await fetchQuizQuestions(title, 'MergeSortDemo');
-      console.log('Sort Type:', sortType);
-      console.log('Title:', title);
+      await fetchQuizQuestions(0);
       setSelectedQuiz(selected);
-      setQuizStarted(true);
-    } else {
-      console.log('Quiz not found or has a different quizId:', quizId);
-    }
-  };
-  const handleStart = () => {
-    setQuizStarted(true);
+      //setQuizStarted(true);
+  } 
+  if (selected && selected.quizId ===1) {
+    await fetchQuizQuestions(1);
+    setSelectedQuiz(selected);
+    //setQuizStarted(true);
+}
+if (selected && selected.quizId ===2) {
+  await fetchQuizQuestions(2);
+  setSelectedQuiz(selected);
+  //setQuizStarted(true);
+}
+if (selected && selected.quizId ===3) {
+  await fetchQuizQuestions(3);
+  setSelectedQuiz(selected);
+  //setQuizStarted(true);
+}else {
+    console.log('Quiz not found or has a different quizId:', quizId);
   }
+  };
 
-  const handleNextQuestion = () => {
+  const calculateScore = () => {
+    let totalScore = 0;
+    const wrongAnswers = [];
+    for (let i = 0; i < quizQuestions.length; i++) {
+      if (userAnswers[i] === quizQuestions[i].answer) { 
+        totalScore++;
+      } else {
+        wrongAnswers.push(i + 1);
+      }
+    }
+    return { totalScore, wrongAnswers };
+  };
+
+  const handleStart = (quizId) => {
+    setQuizStarted(true);
+    onStartQuiz(quizId);
+  };
+
+  const handleNextClick = () => {
     setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
   };
+
+  const handleBackClick = () => {
+    if(currentQuestionIndex>0){
+      setCurrentQuestionIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+
+  const handleQuizSubmit = () => {
+    setQuizSubmitted(true);
+  };
+
+  const unansweredQuestions = () => {
+    const unanswered = [];
+    for (let i = 0; i < quizQuestions.length; i++) {
+      if (userAnswers[i]==null) {
+        unanswered.push(i + 1);
+      }
+    }
+    return unanswered;
+  };
+
 
   const handleAnswerSelection = (questionId, selectedOption) => {
     setUserAnswers((prevAnswers) => ({
@@ -98,85 +148,130 @@ const MergeQuizzesPage = () => {
     }));
   };
 
-  /*return (
-    <div>
-      <Header/>
-      <h1>Practice Quizzes: </h1>
-      {selectedQuiz ? (
-        <div>
-          <h2>Quiz: {selectedQuiz.title}</h2>
-          <button onClick={handleStart}>Start</button>
-          <div>
-            <a href='/practice/merge/quizzes'>Back to Quiz List</a>
-          </div>
-        </div>
-      ) : (
-        // Render the list of quizzes
-        <DBQuizzesList onStartQuiz={onStartQuiz} />
-      )}
+  const renderQuestions = () => {
+    const currentQuestion=quizQuestions[currentQuestionIndex];
+    const currentAnswer=userAnswers[currentQuestionIndex];
+    return(
       <div>
-        <a href='/practice/merge'>Back to Merge-Sort main page</a>
+        <h3>Question {currentQuestionIndex+1}</h3>
+        <p>{currentQuestion.question}</p>
+        <form>
+          {currentQuestion.options.filter((option)=>option).map((option,index)=>(
+            <div key={index}>
+              <input
+                type="radio"
+                id={'option${index}'}
+                value={option}
+                checked={userAnswers[currentQuestionIndex]===option}
+                onChange={()=>handleAnswerSelection(currentQuestionIndex,option)}
+              />
+              <label htmlFor={'option${index}'}>{option}</label>
+            </div>
+          ))}
+        </form>
       </div>
-    
-    </div>
-  );*/
+    );
+  };
+
+  const renderNavigation=()=>{
+    return(
+      <div>
+      <div>
+        <button className='styled-button' onClick={handleBackClick} disabled={currentQuestionIndex===0}>
+          Back
+        </button>
+        <button className='styled-button' onClick={handleNextClick} disabled={currentQuestionIndex===quizQuestions.length-1}>
+          Next
+        </button>
+        </div>
+        <div>
+        {currentQuestionIndex===quizQuestions.length-1 && (
+          <button className='hover-button' onClick={handleQuizSubmit}>Submit Quiz</button>
+        )}
+      </div>
+      </div>
+    );
+  };
+
+  const renderResultsAndFeedback = () => {
+    if (quizSubmitted) {
+      const { totalScore, wrongAnswers } = calculateScore();
+
+      return (
+        <div>
+          <h3>Quiz Results</h3>
+          <p><b>Your Total Score:</b> {totalScore} / {quizQuestions.length}</p>
+
+          {wrongAnswers.length > 0 && (
+            <div>
+              <p><b>Incorrect Answers:</b></p>
+              <ul>
+                {wrongAnswers.map((questionNumber) => (
+                  <li key={questionNumber}>Question {questionNumber}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+
+          {quizQuestions.map((question, index) => (
+            wrongAnswers.includes(index + 1) && (
+              <div key={index}>
+                <h4>Question {index + 1}</h4>
+                <p>Your Answer: {userAnswers[index]}</p>
+                <p>Correct Answer: {question.answer}</p>
+                <p>Explanation: {question.explanation}</p>
+              </div>
+            )
+          ))}
+        </div>
+      );
+    }
+    return null;
+  };
 
   return (
-    <div className="container">
-      <Header className="header" />
-      <h1 className="header">Practice Quizzes</h1>
+    <div>
+      <Header />
+      <h1>Practice Quizzes: </h1>
       {selectedQuiz ? (
-        <div className="quiz-container">
-          <h2 className="header">Quiz: {selectedQuiz.title}</h2>
+        <div className='content'>
+          <h1>Quiz: {selectedQuiz.title}</h1>
+
           {quizStarted ? (
             quizQuestions.length > 0 && currentQuestionIndex < quizQuestions.length ? (
-              <div className="question-container">
-                <h3>Question {currentQuestionIndex + 1}</h3>
-                <p>{quizQuestions[currentQuestionIndex]?.question}</p>
-                <form className="options-container">
-                  {quizQuestions[currentQuestionIndex]?.options.filter((option) => option).map((option, index) => (
-                    <div className="option" key={index}>
-                      <input
-                        type="radio"
-                        id={`option${index}`}
-                        value={option}
-                        checked={userAnswers[quizQuestions[currentQuestionIndex]?.questionId] === option}
-                        onChange={() =>
-                          handleAnswerSelection(quizQuestions[currentQuestionIndex]?.questionId, option)
-                        }
-                      />
-                      <label htmlFor={`option${index}`}>{option}</label>
+              <div className='quiz'>
+                {renderResultsAndFeedback()}
+                {quizSubmitted ? null : (
+                  <div>
+                    {renderQuestions()}
+                    <div>
+                      <p><b>Unanswered Questions: </b>{unansweredQuestions().join(', ')}</p>
                     </div>
-                  ))}
-                </form>
-                <button className="button" onClick={handleNextQuestion}>Next Question</button>
+                    {renderNavigation()}
+                  </div>
+                )}
               </div>
             ) : (
-              <div className="question-container">
-                <h3>Quiz Completed!</h3>
-                <button className="button">Submit</button>
+              <div>
+                <button className='hover-button' onClick={() => handleQuizSubmit()}>Submit</button>
               </div>
             )
           ) : (
-            <button className="button" onClick={() => onStartQuiz(selectedQuiz.quizId)}>Start Quiz</button>
+            <button className='hover-button' onClick={() => handleStart(selectedQuiz.quizId)}>Start Quiz</button>
           )}
-          <div>
-            <a className="link" href='/practice/merge/quizzes'>Back to Quiz List</a>
-          </div>
         </div>
       ) : (
         <DBQuizzesList onStartQuiz={onStartQuiz} />
       )}
+      <div >
+            <a href='/practice/merge/quizzes'>Back to Quiz List</a>
+      </div>
       <div>
-        <a className="link" href='/practice/merge'>Back to Merge-Sort main page</a>
+        <a href='/practice/merge'>Back to Merge-Sort main page</a>
       </div>
     </div>
   );
+
 };
 
 export default MergeQuizzesPage;
-
-
-
-
-
